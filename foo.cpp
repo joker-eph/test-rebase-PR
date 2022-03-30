3a2d856d5c3f942d4116a4a86e295e084c53fbc6
Mehdi Amini <aminim@google.com>
2022-03-29 22:15:27 -0700

    Refactor `foo()` into foo.cpp

---
+++ foo.cpp
@@ -0,0 +1,7 @@
+// Context
+int foo(int a, int b) {
+  return 42; // TODO: implement the right logic
+}
+// Context
+
+
--- init.cpp
+++ init.cpp
@@ -4,10 +4,6 @@ void bar() {
   // context
 }
 
-int foo(int a, int b) {
-  return 42; // TODO: implement the right logic
-}
-
 void baz() {
  // context
 }
